1e27bec3408bd44ae17bcd3f8080ef817f39c66c
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-11 00:14:40 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,4 +1,4 @@
-STUDENT_NAMES = [
+student_names = [
   "Adele",
   "Beyoncé",
   "Cardi B",
@@ -7,26 +7,35 @@ STUDENT_NAMES = [
   "Rihanna"
 ]
 
-def first_student_by_index
+def first_student_by_index(student_names)
   # Write a solution that returns the first student in the array student_names
+  puts student_names[0]
+end
+first_student_by_index
+def fourth_student_by_index(student_names)
+  # Write a solution that returns the first student in the array student_names
+  puts student_names[3]
+  
 end
 
-def fourth_student_by_index
+def last_student_by_index(student_names)
   # Write a solution that returns the first student in the array student_names
+  puts student_names.last
 end
 
-def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
-end
-
-def first_student_by_method
+def first_student_by_method(student_names)
   # Write a solution that returns the first student using the built in .first method
+  puts (student_names).first
 end
 
-def last_student_by_method
+def last_student_by_method(student_names)
   # Write a solution that returns the first student using the built in .last method
+  puts student_names.last
 end
 
-def first_second_and_third_students
+def first_second_and_third_students(student_names)
   # Write a solution that returns the first, second and third students
+  puts (student_names)[0]
+  puts (student_names)[1]
+  puts (student_names)[2]
 end
